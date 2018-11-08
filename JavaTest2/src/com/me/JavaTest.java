8d2a387ff397d63bc6f7f72a3c05134e2de4d5e9
gksaikishore <gksaikishore@gmail.com>
2018-11-08 17:23:00 +0530

    new setup

---
+++ JavaTest2/src/com/me/JavaTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.me;
+
+import com.me.test.Person;
+
+/**
+ * @author SaiKishore
+ *
+ */
+public class JavaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person();
+		System.out.println(p.getName());
+	}
+
+}
